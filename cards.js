3dc0b0dd4b4776dffd33528b37ddb7c68a0bf21c
lily-coder <lilyondie@gmail.com>
2021-08-02 02:32:30 +0300

    Fix linter errors

--- cards.js
+++ cards.js
@@ -1,86 +1,85 @@
 const speakers = [
-    {
-      name: 'Henry Martin',
-      title: 'Director of WedDev Solutions',
-      bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
-      image: {
-        imageUrl: './assets/speaker-1.jpg',
-        imageAlt: 'project preview image',
-      },
+  {
+    name: 'Henry Martin',
+    title: 'Director of WedDev Solutions',
+    bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
+    image: {
+      imageUrl: './assets/speaker-1.jpg',
+      imageAlt: 'project preview image',
     },
-  
-    {
-      name: 'Henry Martin',
-      title: 'Director of WedDev Solutions',
-      bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
-      image: {
-          imageUrl: './assets/speaker-8.jpg',
-          imageAlt: 'project preview image',
-      },
-    },
-  
-    {
-      name: 'Henry Martin',
-      title: 'Director of WedDev Solutions',
-      bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
-      image: {
-        imageUrl: './assets/speaker-3.jpg',
-        imageAlt: 'project preview image',
-      },
-    },
-  
-    {
-      name: 'Henry Martin',
-      title: 'Director of WedDev Solutions',
-      bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
-      image: {
-        imageUrl: './assets/speaker-4.jpg',
-        imageAlt: 'project preview image',
-      },
-    },
+  },
 
-    {
-      name: 'Henry Martin',
-      title: 'Director of WedDev Solutions',
-      bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
-      image: {
-        imageUrl: './assets/speaker-5.jpg',
+  {
+    name: 'Henry Martin',
+    title: 'Director of WedDev Solutions',
+    bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
+    image: {
+        imageUrl: './assets/speaker-8.jpg',
         imageAlt: 'project preview image',
-      },
     },
+  },
 
-    {
-      name: 'Henry Martin',
-      title: 'Director of WedDev Solutions',
-      bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
-      image: {
-        imageUrl: './assets/speaker-6.jpg',
-        imageAlt: 'project preview image',
-      },
+  {
+    name: 'Henry Martin',
+    title: 'Director of WedDev Solutions',
+    bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
+    image: {
+      imageUrl: './assets/speaker-3.jpg',
+      imageAlt: 'project preview image',
     },
-  ];
-  
-  function speakerCards(works) {
-    let cardMenu = `<div class="featured-speaker">
-                        <img src="${works.image.imageUrl}" alt="${works.image.imageUrl}">
-                        <div>
-                            <p class="black">${works.name}</p>
-                            <p class="orange italics">${works.title}</p>
-                            <p>${works.bio}</p>
-                        </div>
-                    </div>`;
-  
-    return cardMenu;
+  },
+
+  {
+    name: 'Henry Martin',
+    title: 'Director of WedDev Solutions',
+    bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
+    image: {
+      imageUrl: './assets/speaker-4.jpg',
+      imageAlt: 'project preview image',
+    },
+  },
+
+  {
+    name: 'Henry Martin',
+    title: 'Director of WedDev Solutions',
+    bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
+    image: {
+      imageUrl: './assets/speaker-5.jpg',
+      imageAlt: 'project preview image',
+    },
+  },
+
+  {
+    name: 'Henry Martin',
+    title: 'Director of WedDev Solutions',
+    bio: 'Henry is an experienced developer who has worked for both large companies and startups. He has a PhD in Computer Science',
+    image: {
+      imageUrl: './assets/speaker-6.jpg',
+      imageAlt: 'project preview image',
+    },
+  },
+];
+
+function speakerCards(works) {
+  let cardMenu = `<div class="featured-speaker">
+                      <img src="${works.image.imageUrl}" alt="${works.image.imageUrl}">
+                      <div>
+                          <p class="black">${works.name}</p>
+                          <p class="orange italics">${works.title}</p>
+                          <p>${works.bio}</p>
+                      </div>
+                  </div>`;
+
+  return cardMenu;
+}
+
+const modalCard = document.querySelector('#my-popup');
+function insertHtml() {
+  const myWorks = Object.keys(speakers);
+  const myWorksLength = Object.keys(speakers).length;
+
+  for (let i = 0; i < myWorksLength; i += 1) {
+    modalCard.innerHTML += speakerCards(speakers[myWorks[i]], i);
   }
-  
-  const modalCard = document.querySelector('#my-popup');
-  function insertHtml() {
-    const myWorks = Object.keys(speakers);
-    const myWorksLength = Object.keys(speakers).length;
-  
-    for (let i = 0; i < myWorksLength; i += 1) {
-      modalCard.innerHTML += speakerCards(speakers[myWorks[i]], i);
-    }
-  }
-  
-  modalCard.onload = insertHtml();
+}
+modalCard.onload = insertHtml();
